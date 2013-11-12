0b6b8e28005a064438b03d802cfa10a58f4c522b
Wade Urry <wader@iwader.co.uk>
2013-11-12 13:42:12 +0000

    Footer template

---
+++ footer.php
@@ -0,0 +1,35 @@
+<footer>
+    <div id="twitter-footer">
+        <div class="container">
+            <a class="logo" href="#">
+                <img src="<?php bloginfo('template_url') ?>/img/twitter-bird-inverse.png">
+            </a>
+        </div>
+    </div>
+
+    <div id="bottom-footer">
+        <div class="container">
+            <div class="footer-nav pull-left hidden-phone">
+                <a href="#">Home</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Contact Me</a>
+            </div>
+
+            <div class="social hidden-phone hidden-tablet">
+                <a href="#" class="social-icon">
+                    <i class="fa fa-bitbucket fa-3x"></i>
+                </a>
+                <a href="#" class="social-icon">
+                    <i class="fa fa-github fa-3x"></i>
+                </a>
+                <a href="#" class="social-icon">
+                    <i class="fa fa-twitter fa-3x"></i>
+                </a>
+            </div>
+
+            <div class="copyright pull-right">
+                &copy; Wade Urry
+            </div>
+        </div>
+    </div>
+</footer>
